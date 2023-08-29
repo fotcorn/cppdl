ab0a077a4c14b712a4fabfea3263bca64a005ff5
fotcorn <fotcorn@gmail.com>
2023-08-29 22:59:47 +0100

    update network weights to use tensors

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <span>
 
+#include "nn_tensor.h"
 #include "tensor.h"
 
 int main() {
-  // init
   tensor<float> t({2, 2});
   tensor o = tensor<float>::ones({2, 2});
 
@@ -23,7 +23,6 @@ int main() {
   std::cout << matrix.add(tensor<float>::constants({3.0f})) << std::endl;
 
   /*
-
   // tensor operations
   tops::mul(t, o);
   tops::exp(t, 3);
@@ -34,18 +33,6 @@ int main() {
   tops::matmul(t1, t2);
 
   tops::reshape(t, {1,2,3});
-
-  // nn
-  tensor<float> input({2, 1});
-
-  tensor<float> l1w({16, 2});
-  tensor<float> l1b({16, 1});
-
-  tensor<float> l2w({16, 16});
-  tensor<float> l2b({16, 1});
-
-  tensor<float> l3w({1, 16});
-  tensor<float> l3b({1, 1});
   */
 
   return 0;

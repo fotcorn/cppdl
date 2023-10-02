6c3fdcf7140cc54c45b8fc8d3dc968b0a8bd641e
fotcorn <fotcorn@gmail.com>
2023-10-02 23:49:39 +0100

    basic forward pass

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,28 @@
 #include <span>
 
 #include "tensor.h"
+#include "utils.h"
 
 #include "data/dataset.h"
 #include "data/nn.h"
 
+template <typename Tensor>
+Tensor net(Tensor input, Tensor w1, Tensor b1, Tensor w2, Tensor b2, Tensor w3,
+           Tensor b3) {
+  auto o1 = (w1.matmul(input) + b1).relu();
+  auto o2 = (w2.matmul(o1) + b2).relu();
+  return w3.matmul(o2) + b3;
+}
+
 int main() {
+  tensor<float> input = tensor<float>::matrix2d(
+      {{DATASET_VALUES[0][0].item()}, {DATASET_VALUES[0][1].item()}});
+  auto result = net<tensor<float>>(input, LAYER_0_WEIGHTS, LAYER_0_BIASES,
+                                   LAYER_1_WEIGHTS, LAYER_1_BIASES,
+                                   LAYER_2_WEIGHTS, LAYER_2_BIASES);
 
-  tensor<float> input = tensor<float>::matrix2d({{1.0f}, {2.0f}});
-
-  // auto o1 = LAYER_0_WEIGHTS.matmul(input);
-  // auto o2 = o1 + LAYER_0_BIASES;
+  std::cout << result[0].item() << std::endl;
+  std::cout << DATASET_LABELS[0].item() << std::endl;
 
   return 0;
 }
---
+++ src/utils.h
@@ -0,0 +1,13 @@
+#include <vector>
+
+template <typename T>
+void printVector(const std::vector<T> &vec) {
+  std::cout << "[";
+  for (size_t i = 0; i < vec.size(); i++) {
+    std::cout << vec[i];
+    if (i != vec.size() - 1) {
+      std::cout << ", ";
+    }
+  }
+  std::cout << "]" << std::endl;
+}

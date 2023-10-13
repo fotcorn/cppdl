dd133940513432b4692afd8d620224f300ba4127
fotcorn <fotcorn@gmail.com>
2023-10-13 22:29:14 +0100

    test 3d elementwise

--- unittests/tensor_elementwise.cpp
+++ unittests/tensor_elementwise.cpp
@@ -39,3 +39,34 @@ TEST(Tensor, ReLU) {
   EXPECT_EQ(0.0f, res[1][1].item());
   EXPECT_EQ(res.getShape(), std::vector<size_t>({2, 2}));
 }
+
+TEST(Tensor, ReLU3D) {
+  tensor<float> a = tensor<float>::matrix2d({{1.0f, -2.0f}, {3.0f, -4.0f}});
+  tensor<float> b = tensor<float>::matrix2d({{-1.0f, 2.0f}, {-3.0f, 4.0f}});
+  tensor<float> c = tensor<float>::stack({a, b}).relu();
+  EXPECT_EQ(1.0f, c[0][0][0].item());
+  EXPECT_EQ(0.0f, c[0][0][1].item());
+  EXPECT_EQ(3.0f, c[0][1][0].item());
+  EXPECT_EQ(0.0f, c[0][1][1].item());
+  EXPECT_EQ(0.0f, c[1][0][0].item());
+  EXPECT_EQ(2.0f, c[1][0][1].item());
+  EXPECT_EQ(0.0f, c[1][1][0].item());
+  EXPECT_EQ(4.0f, c[1][1][1].item());
+  EXPECT_EQ(c.getShape(), std::vector<size_t>({2, 2, 2}));
+}
+
+TEST(Tensor, Add3D) {
+  tensor<float> a = tensor<float>::matrix2d({{1.0f, 2.0f}, {3.0f, 4.0f}});
+  tensor<float> b = tensor<float>::matrix2d({{5.0f, 6.0f}, {7.0f, 8.0f}});
+  tensor<float> c = tensor<float>::stack({a, b});
+  tensor<float> d = c + 1.0f;
+  EXPECT_EQ(2.0f, d[0][0][0].item());
+  EXPECT_EQ(3.0f, d[0][0][1].item());
+  EXPECT_EQ(4.0f, d[0][1][0].item());
+  EXPECT_EQ(5.0f, d[0][1][1].item());
+  EXPECT_EQ(6.0f, d[1][0][0].item());
+  EXPECT_EQ(7.0f, d[1][0][1].item());
+  EXPECT_EQ(8.0f, d[1][1][0].item());
+  EXPECT_EQ(9.0f, d[1][1][1].item());
+  EXPECT_EQ(d.getShape(), std::vector<size_t>({2, 2, 2}));
+}

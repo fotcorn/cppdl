e42b71ca605e178a5dc1a18ba736182d0c5a0a4a
fotcorn <fotcorn@gmail.com>
2023-10-14 16:01:02 +0100

    3d broadcast ops

--- src/tensor.h
+++ src/tensor.h
@@ -7,8 +7,6 @@
 #include <sstream>
 #include <vector>
 
-// todo: implement elementwise with broadcast
-
 template <typename T>
 class tensor final {
 public:
@@ -128,29 +126,23 @@ public:
     auto &op1 = *this;
     auto &op2 = op;
 
-    // Shape is padded by 1's at the front.
+    // Shape and strides are padded by 1's at the front.
     size_t length = std::max(op1.shape.size(), op2.shape.size());
+
     auto shapeOp1 = std::vector<size_t>(length, 1);
     auto shapeOp2 = std::vector<size_t>(length, 1);
     std::copy_backward(op1.shape.begin(), op1.shape.end(), shapeOp1.end());
     std::copy_backward(op2.shape.begin(), op2.shape.end(), shapeOp2.end());
 
-    // Strides are padded by 1's at the back.
-    auto strideOp1 = op1.strides;
-    auto strideOp2 = op2.strides;
-    ssize_t diff = strideOp1.size() - strideOp2.size();
-    if (diff > 0) {
-      for (ssize_t i = 0; i < diff; i++) {
-        strideOp2.push_back(1);
-      }
-    } else if (diff < 0) {
-      for (ssize_t i = diff; i < 0; i++) {
-        strideOp1.push_back(1);
-      }
-    }
+    auto stridesOp1 = std::vector<size_t>(length, 1);
+    auto stridesOp2 = std::vector<size_t>(length, 1);
+    std::copy_backward(op1.strides.begin(), op1.strides.end(),
+                       stridesOp1.end());
+    std::copy_backward(op2.strides.begin(), op2.strides.end(),
+                       stridesOp2.end());
 
     assert(shapeOp1.size() == shapeOp2.size());
-    assert(strideOp1.size() == strideOp2.size());
+    assert(stridesOp1.size() == stridesOp2.size());
 
     for (size_t i = 0; i < shapeOp1.size(); i++) {
       if (!(shapeOp1[i] == shapeOp2[i] || shapeOp1[i] == 1 ||
@@ -166,8 +158,8 @@ public:
       size_t maxDim = std::max(dimOp1, dimOp2);
       tensor<T> res = tensor<T>({maxDim});
       for (size_t i = 0; i < maxDim; i++) {
-        res.data[i] = op1.data[op1.offset + strideOp1[0] * (i % dimOp1)] +
-                      op2.data[op2.offset + strideOp2[0] * (i % dimOp2)];
+        res.data[i] = op1.data[op1.offset + stridesOp1[0] * (i % dimOp1)] +
+                      op2.data[op2.offset + stridesOp2[0] * (i % dimOp2)];
       }
       return res;
     }
@@ -179,12 +171,35 @@ public:
       tensor<T> res = tensor<T>({dim0Max, dim1Max});
       for (size_t dim0 = 0; dim0 < dim0Max; dim0++) {
         for (size_t dim1 = 0; dim1 < dim1Max; dim1++) {
-          T index1 = op1.offset + (dim0 % shapeOp1[0]) * strideOp1[0] +
-                     (dim1 % shapeOp1[1]) * strideOp1[1];
-          T index2 = op2.offset + (dim0 % shapeOp2[0]) * strideOp2[0] +
-                     (dim1 % shapeOp2[1]) * strideOp2[1];
+          T index1 = op1.offset + (dim0 % shapeOp1[0]) * stridesOp1[0] +
+                     (dim1 % shapeOp1[1]) * stridesOp1[1];
+          T index2 = op2.offset + (dim0 % shapeOp2[0]) * stridesOp2[0] +
+                     (dim1 % shapeOp2[1]) * stridesOp2[1];
           res.data[dim0 * res.strides[0] + dim1] =
               func(op1.data[index1], op2.data[index2]);
+        }
+      }
+      return res;
+    }
+
+    if (shapeOp1.size() == 3) {
+      size_t dim0Max = std::max(shapeOp1[0], shapeOp2[0]);
+      size_t dim1Max = std::max(shapeOp1[1], shapeOp2[1]);
+      size_t dim2Max = std::max(shapeOp1[2], shapeOp2[2]);
+
+      tensor<T> res = tensor<T>({dim0Max, dim1Max, dim2Max});
+      for (size_t dim0 = 0; dim0 < dim0Max; dim0++) {
+        for (size_t dim1 = 0; dim1 < dim1Max; dim1++) {
+          for (size_t dim2 = 0; dim2 < dim2Max; dim2++) {
+            T index1 = op1.offset + (dim0 % shapeOp1[0]) * stridesOp1[0] +
+                       (dim1 % shapeOp1[1]) * stridesOp1[1] +
+                       (dim2 % shapeOp1[2]) * stridesOp1[2];
+            T index2 = op2.offset + (dim0 % shapeOp2[0]) * stridesOp2[0] +
+                       (dim1 % shapeOp2[1]) * stridesOp2[1] +
+                       (dim2 % shapeOp2[2]) * stridesOp2[2];
+            res.data[dim0 * res.strides[0] + dim1 * res.strides[1] + dim2] =
+                func(op1.data[index1], op2.data[index2]);
+          }
         }
       }
       return res;
--- unittests/tensor_broadcast.cpp
+++ unittests/tensor_broadcast.cpp
@@ -98,3 +98,54 @@ TEST(Tensor, Matrix2DAdd22_22) {
   EXPECT_EQ(10.5f, res[1][1].item());
   EXPECT_EQ(res.getShape(), std::vector<size_t>({2, 2}));
 }
+
+TEST(Tensor, Matrix3DAdd_1_22) {
+  tensor<float> a = tensor<float>::stack(
+      {tensor<float>::matrix2d({{1.0f, 2.0f}, {3.0f, 4.0f}}),
+       tensor<float>::matrix2d({{5.0f, 6.0f}, {7.0f, 8.0f}})});
+  tensor<float> b = tensor<float>::matrix2d({{1.0f}});
+  tensor<float> res = a + b;
+  EXPECT_EQ(2.0f, res[0][0][0].item());
+  EXPECT_EQ(3.0f, res[0][0][1].item());
+  EXPECT_EQ(4.0f, res[0][1][0].item());
+  EXPECT_EQ(5.0f, res[0][1][1].item());
+  EXPECT_EQ(6.0f, res[1][0][0].item());
+  EXPECT_EQ(7.0f, res[1][0][1].item());
+  EXPECT_EQ(8.0f, res[1][1][0].item());
+  EXPECT_EQ(9.0f, res[1][1][1].item());
+  EXPECT_EQ(res.getShape(), std::vector<size_t>({2, 2, 2}));
+}
+
+TEST(Tensor, Matrix3DAdd_12_22) {
+  tensor<float> a = tensor<float>::stack(
+      {tensor<float>::matrix2d({{1.0f, 2.0f}, {3.0f, 4.0f}}),
+       tensor<float>::matrix2d({{5.0f, 6.0f}, {7.0f, 8.0f}})});
+  tensor<float> b = tensor<float>::matrix2d({{1.0f, 2.0f}});
+  tensor<float> res = a + b;
+  EXPECT_EQ(2.0f, res[0][0][0].item());
+  EXPECT_EQ(4.0f, res[0][0][1].item());
+  EXPECT_EQ(4.0f, res[0][1][0].item());
+  EXPECT_EQ(6.0f, res[0][1][1].item());
+  EXPECT_EQ(6.0f, res[1][0][0].item());
+  EXPECT_EQ(8.0f, res[1][0][1].item());
+  EXPECT_EQ(8.0f, res[1][1][0].item());
+  EXPECT_EQ(10.0f, res[1][1][1].item());
+  EXPECT_EQ(res.getShape(), std::vector<size_t>({2, 2, 2}));
+}
+
+TEST(Tensor, Matrix3DAdd_22_22) {
+  tensor<float> a = tensor<float>::stack(
+      {tensor<float>::matrix2d({{1.0f, 2.0f}, {3.0f, 4.0f}}),
+       tensor<float>::matrix2d({{5.0f, 6.0f}, {7.0f, 8.0f}})});
+  tensor<float> b = tensor<float>::matrix2d({{1.0f, 2.0f}, {3.0f, 4.0f}});
+  tensor<float> res = a + b;
+  EXPECT_EQ(2.0f, res[0][0][0].item());
+  EXPECT_EQ(4.0f, res[0][0][1].item());
+  EXPECT_EQ(6.0f, res[0][1][0].item());
+  EXPECT_EQ(8.0f, res[0][1][1].item());
+  EXPECT_EQ(6.0f, res[1][0][0].item());
+  EXPECT_EQ(8.0f, res[1][0][1].item());
+  EXPECT_EQ(10.0f, res[1][1][0].item());
+  EXPECT_EQ(12.0f, res[1][1][1].item());
+  EXPECT_EQ(res.getShape(), std::vector<size_t>({2, 2, 2}));
+}

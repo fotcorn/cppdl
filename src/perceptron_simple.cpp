49b144719d6300a43509da5e0e561738efadb89f
fotcorn <fotcorn@gmail.com>
2024-01-05 17:57:12 +0000

    use tanh activation on hidden layer to get a solution most of the time

--- src/perceptron_simple.cpp
+++ src/perceptron_simple.cpp
@@ -1,4 +1,3 @@
-
 #include <cmath>
 #include <random>
 
@@ -12,12 +11,13 @@ private:
   std::uniform_real_distribution<> dis;
 
 public:
+  Random() : gen(rd()), dis(0, 1.0) {}
   Random(unsigned int seed) : gen(seed), dis(0, 1.0) {}
 
   float getFloatZeroOne() { return dis(gen); }
 };
 
-constexpr float LEARNING_RATE = 0.01;
+constexpr float LEARNING_RATE = 0.005;
 constexpr int NUM_SAMPLES = 4;
 
 struct Dataset {
@@ -28,7 +28,6 @@ struct Dataset {
 void learn(float groundTruth[NUM_SAMPLES]);
 
 int main() {
-
   Dataset datasets[] = {{"or", {0, 1, 1, 1}},  {"nor", {1, 0, 0, 0}},
                         {"and", {0, 0, 0, 1}}, {"nand", {1, 1, 1, 0}},
                         {"xor", {0, 1, 1, 0}}, {"nxor", {1, 0, 0, 1}}};
@@ -40,7 +39,7 @@ int main() {
 }
 
 void learn(float groundTruth[NUM_SAMPLES]) {
-  Random r(42);
+  Random r;
 
   float l1p1w1 = r.getFloatZeroOne();
   float l1p1w2 = r.getFloatZeroOne();
@@ -57,22 +56,21 @@ void learn(float groundTruth[NUM_SAMPLES]) {
   float input[NUM_SAMPLES][2] = {{0, 0}, {1, 0}, {0, 1}, {1, 1}};
 
   int correct, epoch;
-  for (epoch = 0; epoch < 100000; epoch++) {
+  for (epoch = 0; epoch < 1000000; epoch++) {
     correct = 0;
     for (int i = 0; i < NUM_SAMPLES; i++) {
       // Perceptron with bias
       float a1 = input[i][0] * l1p1w1;
       float a2 = input[i][1] * l1p1w2;
-      float p1Res = a1 + a2 + l1p1b;
+      float p1Res = std::tanh(a1 + a2 + l1p1b);
 
       float a3 = input[i][0] * l1p2w1;
       float a4 = input[i][1] * l1p2w2;
-      float p2Res = a3 + a4 + l1p2b;
+      float p2Res = std::tanh(a3 + a4 + l1p2b);
 
-      float result = p1Res * l2w1 + p2Res * l2w2 + l2b;
+      float result = p1Res * l2w1 + p2Res * l2w2 + l2b > 0 ? 1 : 0;
 
-      if ((result > 0 && groundTruth[i] == 1) ||
-          (result <= 0 && groundTruth[i] == 0)) {
+      if (result == groundTruth[i]) {
         correct++;
       }
 

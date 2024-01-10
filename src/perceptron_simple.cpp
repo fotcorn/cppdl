c6d87f46b451b32f56271398ca2749ecc70894b9
fotcorn <fotcorn@gmail.com>
2024-01-10 21:58:49 +0000

    cleaned up mlp testing

--- src/perceptron_simple.cpp
+++ src/perceptron_simple.cpp
@@ -17,16 +17,30 @@ public:
   float getFloatZeroOne() { return dis(gen); }
 };
 
-float tanh_deriv(float value) {
-  float temp = std::tanh(value);
-  return 1.0f - temp * temp;
-}
+constexpr float LEARNING_RATE = 0.01;
+constexpr int NUM_SAMPLES = 4;
+
+struct Dataset {
+  const char *name;
+  float groundTruth[NUM_SAMPLES];
+};
+
+void learn(float groundTruth[NUM_SAMPLES]);
 
 int main() {
-  Random r(42);
 
-  constexpr float LEARNING_RATE = 0.01;
-  constexpr int NUM_SAMPLES = 4;
+  Dataset datasets[] = {{"or", {0, 1, 1, 1}},  {"nor", {1, 0, 0, 0}},
+                        {"and", {0, 0, 0, 1}}, {"nand", {1, 1, 1, 0}},
+                        {"xor", {0, 1, 1, 0}}, {"nxor", {1, 0, 0, 1}}};
+
+  for (Dataset &dataset : datasets) {
+    fmt::println("Dataset: {}", dataset.name);
+    learn(dataset.groundTruth);
+  }
+}
+
+void learn(float groundTruth[NUM_SAMPLES]) {
+  Random r(42);
 
   float l1p1w1 = r.getFloatZeroOne();
   float l1p1w2 = r.getFloatZeroOne();
@@ -41,68 +55,52 @@ int main() {
   float l2b = r.getFloatZeroOne();
 
   float input[NUM_SAMPLES][2] = {{0, 0}, {1, 0}, {0, 1}, {1, 1}};
-  // float groundTruth[NUM_SAMPLES] = {0, 1, 1, 1}; // or
-  float groundTruth[NUM_SAMPLES] = {1, 0, 0, 0}; // nor
-  // float groundTruth[NUM_SAMPLES] = {0, 0, 0, 1}; // and
-  // float groundTruth[NUM_SAMPLES] = {1, 1, 1, 0}; // nand
-  // float groundTruth[NUM_SAMPLES] = {0, 1, 1, 0}; // xor
-  // float groundTruth[NUM_SAMPLES] = {1, 0, 0, 1}; // nxor
 
-  for (int epoch = 0; epoch < 2000; epoch++) {
-    int correct = 0;
+  int correct, epoch;
+  for (epoch = 0; epoch < 100000; epoch++) {
+    correct = 0;
     for (int i = 0; i < NUM_SAMPLES; i++) {
       // Perceptron with bias
       float a1 = input[i][0] * l1p1w1;
       float a2 = input[i][1] * l1p1w2;
       float p1Res = a1 + a2 + l1p1b;
-      float p1Activation = std::tanh(p1Res);
 
       float a3 = input[i][0] * l1p2w1;
       float a4 = input[i][1] * l1p2w2;
       float p2Res = a3 + a4 + l1p2b;
-      float p2Activation = std::tanh(p2Res);
 
-      float p3Res = p1Res * l2w1 + p2Res * l2w2 + l2b;
-      float result = std::tanh(p3Res);
+      float result = p1Res * l2w1 + p2Res * l2w2 + l2b;
 
-      if ((result > 0.5 && groundTruth[i] > 0.5) ||
-          (result < 0.5 && groundTruth[i] < 0.5)) {
+      if ((result > 0 && groundTruth[i] == 1) ||
+          (result <= 0 && groundTruth[i] == 0)) {
         correct++;
       }
 
-      float p3ActivationLoss = groundTruth[i] - result;
-      float p3loss = tanh_deriv(p3ActivationLoss);
+      float loss = groundTruth[i] - result;
 
       // Back propagation
-      float l2w1Grad = p1Activation * p3loss;
-      float l2w2Grad = p2Activation * p3loss;
+      float l2w1Grad = p1Res * loss;
+      float l2w2Grad = p2Res * loss;
 
       // Gradient descent
-      l2b += p3loss * LEARNING_RATE;
+      l2b += loss * LEARNING_RATE;
       l2w1 += l2w1Grad * LEARNING_RATE;
       l2w2 += l2w2Grad * LEARNING_RATE;
 
-      ////
+      l1p1b += (l2w1Grad + l2w2Grad) * LEARNING_RATE;
+      l1p2b += (l2w1Grad + l2w2Grad) * LEARNING_RATE;
 
-      float grad = tanh_deriv(l2w1Grad) + tanh_deriv(l2w2Grad);
+      l1p1w1 += input[i][0] * (l2w1Grad + l2w2Grad) * LEARNING_RATE;
+      l1p1w2 += input[i][1] * (l2w1Grad + l2w2Grad) * LEARNING_RATE;
 
-      l1p1b += grad * LEARNING_RATE;
-      l1p2b += grad * LEARNING_RATE;
-
-      l1p1w1 += input[i][0] * grad * LEARNING_RATE;
-      l1p1w2 += input[i][1] * grad * LEARNING_RATE;
-
-      l1p2w1 += input[i][0] * grad * LEARNING_RATE;
-      l1p2w2 += input[i][1] * grad * LEARNING_RATE;
-
-      fmt::println("l1p1w1: {}, l1p1w2: {}, l1p1b: {}, l1p2w1: {}, l1p2w2: {}, "
-                   "l1p2b: {}, l2w1: {}, l2w2: {}, l2b: {}",
-                   l1p1w1, l1p1w2, l1p1b, l1p2w1, l1p2w2, l1p2b, l2w1, l2w2,
-                   l2b);
+      l1p2w1 += input[i][0] * (l2w1Grad + l2w2Grad) * LEARNING_RATE;
+      l1p2w2 += input[i][1] * (l2w1Grad + l2w2Grad) * LEARNING_RATE;
     }
     if (correct == NUM_SAMPLES) {
-      fmt::println("100% accuracy after {} epochs", epoch);
-      break;
+      fmt::println("Solution found after {} epochs", epoch);
+      return;
     }
   }
+
+  fmt::println("No solution found after {} epochs", epoch);
 }

c4c91afb563c306ca5d08cefa7bcd430e3048e28
fotcorn <fotcorn@gmail.com>
2024-03-08 22:22:50 +0000

    refactor build to make cppdl an actual library

--- examples/mnist.cpp
+++ examples/mnist.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 
+#include "cppdl/mnist_utils.h"
 #include "cppdl/nn.h"
 #include "cppdl/serialization.h"
 
@@ -12,47 +13,6 @@ constexpr float initialLearningRate = 0.001f;
 constexpr int lrDecayEpoch = 30;
 constexpr float lrDecayRate = 0.5f;
 constexpr size_t batchSize = 200;
-
-std::vector<Tensor<float>> loadImages(std::string path) {
-  std::ifstream file(path, std::ios::binary);
-  if (!file.is_open()) {
-    throw std::runtime_error(fmt::format("Cannot load image file {}", path));
-  }
-
-  file.seekg(16, std::ios::beg); // Skip header.
-  std::vector<Tensor<float>> images;
-
-  unsigned char buffer[imageSize];
-  while (file.read(reinterpret_cast<char *>(buffer), imageSize)) {
-    float image[imageSize];
-    for (size_t i = 0; i < imageSize; ++i) {
-      image[i] = buffer[i] / 255.0f;
-    }
-    images.push_back(Tensor<float>::vector(std::begin(image), std::end(image)));
-  }
-  return images;
-}
-
-std::vector<Tensor<float>> loadLabels(std::string path) {
-  std::ifstream file(path, std::ios::binary);
-  if (!file.is_open()) {
-    throw std::runtime_error(fmt::format("Cannot load labels file {}", path));
-  }
-
-  file.seekg(8, std::ios::beg); // Skip header.
-
-  std::vector<Tensor<float>> labels;
-
-  char label;
-
-  while (file.read(&label, 1)) {
-    float labelOneHot[10] = {};
-    labelOneHot[static_cast<size_t>(label)] = 1.0f;
-    labels.emplace_back(
-        Tensor<float>::vector(std::begin(labelOneHot), std::end(labelOneHot)));
-  }
-  return labels;
-}
 
 int main() {
   auto rawTrainImages = loadImages("../data/mnist/train-images-idx3-ubyte");
